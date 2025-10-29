a3d54e1d2f1b5514d321433b16daf7784ad7c5ef
amro <amro@gmail.com>
2025-10-29 04:13:09 +0400

    Adding reset forget password fucntionalluty5

--- app/Notifications/PasswordResetCodeNotification.php
+++ app/Notifications/PasswordResetCodeNotification.php
@@ -2,14 +2,11 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class PasswordResetCodeNotification extends Notification implements ShouldQueue
+class PasswordResetCodeNotification extends Notification
 {
-    use Queueable;
 
     /**
      * Create a new notification instance.
