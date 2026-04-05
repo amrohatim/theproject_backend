0800eaa35760619f50981d4ded833da4e8f40196
amrohatim <gogoh3296@gmail.com>
2026-04-05 20:04:22 +0400

    redesign the register steps with images 2

--- resources/views/auth/provider-register-step1.blade.php
+++ resources/views/auth/provider-register-step1.blade.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Data3Chic - {{ __('messages.provider_registration') }}</title>
+    <title>Glowlabs - {{ __('messages.provider_registration') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 
